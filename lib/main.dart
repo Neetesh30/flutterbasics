f39205c0887d62ec5e82125abe078fb894cefb69
Neetesh30 <neetesh652@gmail.com>
2020-09-30 20:53:29 +0530

    "Basic Flutter App with drawer and list completed"

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -5,6 +6,7 @@ void main(){
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,6 +18,36 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Text("Hello To flutters "),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                  child: UserAccountsDrawerHeader(
+                      accountName: Text("Neetesh"),
+                      accountEmail: Text("xyz@test.com"),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent
+                  ),
+              ),
+              ListTile(
+                title: Text("List No 1"),
+                subtitle: Text("my sub list  information"),
+                leading: Icon(Icons.person),
+                onTap: (){
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text("List No 2"),
+                subtitle: Text("my sub list  information"),
+                leading: Icon(Icons.star),
+                trailing: Icon(Icons.more_vert),
+              ),
+            ],
+          ),
         ),
       ),
     );
